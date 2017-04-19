e77609ef3437585d9652b06562298a3e260e0657
iam4x <iam4x@me.com>
2017-04-19 02:13:01 +0200

    feat: display directory && git infos

---
+++ zsh-iterm-touchbar.zsh
@@ -0,0 +1,37 @@
+update_touchbar() {
+  # Reset Touchbar
+  echo -ne "\033]1337;PopKeyLabels\a"
+
+  # CURRENT_DIR
+  # -----------
+  echo -ne "\033]1337;SetKeyLabel=F1=👉 $(echo $(pwd) | awk -F/ '{print $(NF-1)"/"$(NF)}')\a"
+
+  # GIT
+  # ---
+  # Check if the current directory is in a Git repository.
+  # Check if the current directory is in a Git repository.
+  command git rev-parse --is-inside-work-tree &>/dev/null || return
+
+  # Check if the current directory is in .git before running git checks.
+  if [[ "$(git rev-parse --is-inside-git-dir 2> /dev/null)" == 'false' ]]; then
+
+    # Ensure the index is up to date.
+    git update-index --really-refresh -q &>/dev/null
+
+    # String of indicators
+    local indicators=''
+
+    indicators+="$(spaceship_git_uncomitted)"
+    indicators+="$(spaceship_git_unstaged)"
+    indicators+="$(spaceship_git_untracked)"
+    indicators+="$(spaceship_git_stashed)"
+    indicators+="$(spaceship_git_unpushed_unpulled)"
+
+    [ -n "${indicators}" ] && touchbarIndicators="🔥[${indicators}]" || touchbarIndicators="🙌";
+
+    echo -ne "\033]1337;SetKeyLabel=F2=🎋 $(git_current_branch)\a"
+    echo -ne "\033]1337;SetKeyLabel=F3=$touchbarIndicators\a"
+  fi
+}
+
+PS1+='$(update_touchbar)'
