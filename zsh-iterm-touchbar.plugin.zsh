fc9f4076962deafbfde684a83b18f690ad5dcc41
iam4x <iam4x@me.com>
2017-04-19 16:05:15 +0200

    fix: missing functions for git

--- zsh-iterm-touchbar.plugin.zsh
+++ zsh-iterm-touchbar.plugin.zsh
@@ -1,3 +1,64 @@
+# GIT
+GIT_UNCOMMITTED="${GIT_UNCOMMITTED:-+}"
+GIT_UNSTAGED="${GIT_UNSTAGED:-!}"
+GIT_UNTRACKED="${GIT_UNTRACKED:-?}"
+GIT_STASHED="${GIT_STASHED:-$}"
+GIT_UNPULLED="${GIT_UNPULLED:-⇣}"
+GIT_UNPUSHED="${GIT_UNPUSHED:-⇡}"
+
+# Uncommitted changes.
+# Check for uncommitted changes in the index.
+git_uncomitted() {
+  if ! $(git diff --quiet --ignore-submodules --cached); then
+    echo -n "${GIT_UNCOMMITTED}"
+  fi
+}
+
+# Unstaged changes.
+# Check for unstaged changes.
+git_unstaged() {
+  if ! $(git diff-files --quiet --ignore-submodules --); then
+    echo -n "${GIT_UNSTAGED}"
+  fi
+}
+
+# Untracked files.
+# Check for untracked files.
+git_untracked() {
+  if [ -n "$(git ls-files --others --exclude-standard)" ]; then
+    echo -n "${GIT_UNTRACKED}"
+  fi
+}
+
+# Stashed changes.
+# Check for stashed changes.
+git_stashed() {
+  if $(git rev-parse --verify refs/stash &>/dev/null); then
+    echo -n "${GIT_STASHED}"
+  fi
+}
+
+# Unpushed and unpulled commits.
+# Get unpushed and unpulled commits from remote and draw arrows.
+git_unpushed_unpulled() {
+  # check if there is an upstream configured for this branch
+  command git rev-parse --abbrev-ref @'{u}' &>/dev/null || return
+
+  local count
+  count="$(command git rev-list --left-right --count HEAD...@'{u}' 2>/dev/null)"
+  # exit if the command failed
+  (( !$? )) || return
+
+  # counters are tab-separated, split on tab and store as array
+  count=(${(ps:\t:)count})
+  local arrows left=${count[1]} right=${count[2]}
+
+  (( ${right:-0} > 0 )) && arrows+="${GIT_UNPULLED}"
+  (( ${left:-0} > 0 )) && arrows+="${GIT_UNPUSHED}"
+
+  [ -n $arrows ] && echo -n "${arrows}"
+}
+
 precmd() {
   # Reset Touchbar
   echo -ne "\033]1337;PopKeyLabels\a"
@@ -20,11 +81,11 @@ precmd() {
     # String of indicators
     local indicators=''
 
-    indicators+="$(spaceship_git_uncomitted)"
-    indicators+="$(spaceship_git_unstaged)"
-    indicators+="$(spaceship_git_untracked)"
-    indicators+="$(spaceship_git_stashed)"
-    indicators+="$(spaceship_git_unpushed_unpulled)"
+    indicators+="$(git_uncomitted)"
+    indicators+="$(git_unstaged)"
+    indicators+="$(git_untracked)"
+    indicators+="$(git_stashed)"
+    indicators+="$(git_unpushed_unpulled)"
 
     [ -n "${indicators}" ] && touchbarIndicators="🔥[${indicators}]" || touchbarIndicators="🙌";
 
